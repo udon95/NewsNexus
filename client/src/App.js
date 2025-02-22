91662369c73c4d8061b7472afe953bb504268e0e
LilDevil181818 <graceliedevinna@gmail.com>
2025-02-22 22:33:02 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import SubscriptionPage from "./pages/SubscriptionPage";
 import TopicInterestsPage from "./pages/TopicInterestsPage";
+import ViewRoomsPage from "./pages/ViewRoomsPage";
+import RoomPage from "./pages/RoomPage"; // Import the Room Page
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/topics" />} /> {/* Redirect to topics page */}
+        <Route path="/" element={<Navigate to="/rooms" />} />
         <Route path="/subscription" element={<SubscriptionPage />} />
         <Route path="/topics" element={<TopicInterestsPage />} />
+        <Route path="/rooms" element={<ViewRoomsPage />} />
+        <Route path="/room/:id" element={<RoomPage />} /> {/* Handles individual rooms */}
       </Routes>
     </Router>
   );
