c3851e88cc7a8f9919208d6affbb94177cbd84bc
Cher2803 <cherwei.ong@gmail.com>
2025-03-27 16:47:31 +0800

    Delete server/pern-auth/backend directory

--- server/pern-auth/backend/middleware/roleMiddleware.js
+++
@@ -1,9 +0,0 @@
-const adminOnly = (req, res, next) => {
-    if (req.user.role !== "admin") {
-      return res.status(403).json({ message: "Access denied. Admins only." });
-    }
-    next();
-  };
-  
-  module.exports = adminOnly;
-  
