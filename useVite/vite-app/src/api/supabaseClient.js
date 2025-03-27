b79496bed1858ef8de9745595affdfc3b9eac3e2
Cher2803 <cherwei.ong@gmail.com>
2025-03-27 16:48:58 +0800

    Delete useVite directory

--- useVite/vite-app/src/api/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-// Load from environment variables
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
-
-export default supabase;
