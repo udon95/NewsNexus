8fccb9f3d0b73a2d0b61162c5446ba6a830a3ebc
udon95 <vanessawee2000@gmail.com>
2025-04-08 11:21:20 +0800

    Content moderation working
    Image moderation to be tested

--- NewsNexus/server/db/db.js
+++ NewsNexus/server/db/db.js
@@ -1,6 +1,6 @@
-const { createClient } = require("@supabase/supabase-js");
-require("dotenv").config();
+// const { createClient } = require("@supabase/supabase-js");
+// require("dotenv").config();
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+// const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
-module.exports = supabase;
+// module.exports = supabase;
