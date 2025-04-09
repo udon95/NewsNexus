a315e5f63a265e82741746c9ba70b395e2b40874
hzplus <hongzhetee@gmail.com>
2025-04-10 00:52:22 +0800

    Create Layout.jsx

---
+++ client/src/Layout.jsx
@@ -0,0 +1,28 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/header";
+
+const Layout = () => {
+  return (
+    <div className="w-full min-h-screen flex flex-col bg-gray-100">
+      <Header />
+
+      <main className="flex-grow flex flex-col items-center justify-center w-full px-4 sm:px-6">
+        <Outlet />
+      </main>
+
+      <footer className="w-full h-16 bg-[#7FB0FE] text-sm sm:text-base text-[#00317F] font-bold flex items-center justify-left px-20">
+        <p>&copy; 2025 NewsNexus. All Rights Reserved. &nbsp; &nbsp; &nbsp;</p>
+        <a href="/privacy" className="underline">
+          {" "}
+          Privacy Policy
+        </a>
+        <span>&nbsp; &nbsp; | &nbsp; &nbsp;</span> 
+        <a href="/guidelines" className="underline">
+          Platform Guidelines
+        </a>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
