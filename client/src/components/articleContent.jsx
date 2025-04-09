7f00be23dc42ce8aed9219928631e13abbc4ef35
hzplus <hongzhetee@gmail.com>
2025-04-10 00:50:24 +0800

    Update articleContent.jsx

--- client/src/components/articleContent.jsx
+++ client/src/components/articleContent.jsx
@@ -1,40 +1,49 @@
 import React from "react";
-import articleImage from "../assets/lhl.png"; // Import the article image
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const ArticleContent = () => {
+const ArticleContent = ({ articleRef, title, text, imagepath, postDate, author }) => {
+  const navigate = useNavigate();
+
   return (
-    <div className="flex flex-col w-full px-4 sm:px-8 py-10 mx-auto max-w-4xl">
-      {/* Article Title */}
+    <div className="flex flex-col w-full px-4 sm:px-8 mx-auto max-w-4xl font-grotesk">
+      {/* Title */}
       <h1 className="font-grotesk text-4xl sm:text-5xl font-bold text-black text-left">
-        Personal Legacy of Lee Hsien Loong
+        {title}
       </h1>
 
-      {/* Article Image */}
-      <img src={articleImage} alt="Article" className="w-full rounded-lg my-6 bg-gray-300" />
+      {/* Image at the top */}
+      {imagepath && (
+        <img
+          src={imagepath}
+          alt="Article"
+          className="w-full h-auto object-cover my-6 bg-gray-300"  // Full width, maintain aspect ratio
+        />
+      )}
 
-      {/* 🔄 Moved Date to Bottom Left */}
+      {/* Post Meta */}
       <span className="text-lg text-[#00317F] mb-4 self-start">
-        Posted on 22/01/2025
+        Posted by{" "}
+        <Link
+          to={`/public-profile/${author?.userid}`}
+          className="underline hover:text-blue-600"
+        >
+          {author?.username}
+        </Link>{" "}
+        on {postDate}
       </span>
 
-      {/* Expanded Article Content */}
-      <div className="text-lg sm:text-xl font-medium text-black leading-relaxed">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus convallis odio
-          at lacus fermentum, sit amet molestie dolor convallis. Quisque imperdiet nibh in
-          nisl congue, non vehicula lectus tristique.
-        </p>
-        <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-          laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-          architecto beatae vitae dicta sunt explicabo.
-        </p>
-        <p>
-          At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-          voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint
-          occaecati cupiditate non provident.
-        </p>
-      </div>
+      {/* Article Body */}
+      <div
+        ref={articleRef}
+        className="text-lg sm:text-xl font-medium text-black leading-relaxed space-y-6"
+        dangerouslySetInnerHTML={{
+          __html: text.replace(
+            /<img/g,
+            '<img class="w-full h-auto object-cover my-6 bg-gray-300"'
+          )
+        }}
+      />
     </div>
   );
 };
