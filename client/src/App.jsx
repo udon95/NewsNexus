cebe224aaeb86fa34c5477ba4843b62c5eb157b6
hzplus <hongzhetee@gmail.com>
2025-04-11 01:24:40 +0800

    Update App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,83 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import Login from "./pages/login.jsx";
+import Register from "./pages/register.jsx";
+import Home from "./pages/home.jsx";
+import Layout from "./Layout.jsx";
+import Subscription from "./pages/subscription.jsx";
+import Explore from "./pages/explore.jsx";
+import Guidelines from "./pages/guidelines.jsx";
+import Privacy from "./pages/privacy.jsx";
+import Latest from "./components/latestNews.jsx";
+import Article from "./pages/article.jsx";
+import FreeDashboard from "./components/freeUser/freeDashboard.jsx";
+import PremiumDashboard from "./components/premiumUser/premiumDashboard.jsx";
+import AdminDashboard from "./components/adminUser/adminDashboard.jsx";
+import ForgotPassword from "./pages/forgetPW";
+import ResetPassword from "./pages/resetPW.jsx";
+import ViewRooms from "./components/premiumUser/viewRooms.jsx";
+import RoomPage from "./components/premiumUser/roomPage.jsx";
+import SubscriptionStatus from "./components/payment.jsx";
+import PublicProfile from "./components/publicProfile.jsx";
+import FactCheck from "./components/factCheck.jsx";
+
+import supabase from "./api/supabaseClient.js";
+import "./index.css";
+
+function App() {
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      async (event, session) => {
+        if (event === "SIGNED_IN" && session?.user?.email_confirmed_at) {
+          const userId = session.user.id;
+
+          //  Update user status to "Free" after email verification
+          await supabase
+            .from("users")
+            .update({ status: "Free" })
+            .eq("auth_id", userId);
+        }
+      }
+    );
+
+    return () => {
+      authListener?.subscription.unsubscribe();
+    };
+  }, []);
+
+  return (
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="/subscription" element={<Subscription />} />
+          <Route
+            path="/subscription-status/:status"
+            element={<SubscriptionStatus />}
+          />
+
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/guidelines" element={<Guidelines />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/latest" element={<Latest />} />
+          <Route path="/article/:articleName" element={<Article />} />
+          <Route path="/public-profile/:userid" element={<PublicProfile />} />
+
+          <Route path="/freeDashboard/*" element={<FreeDashboard />} />
+          <Route path="/premiumDashboard/*" element={<PremiumDashboard />} />
+          <Route path="/adminDashboard/*" element={<AdminDashboard />} />
+
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+
+          <Route path="/rooms" element={<ViewRooms />} />
+          <Route path="/room/:id" element={<RoomPage />} />
+          
+          <Route path="/factcheck" element={<FactCheck/>}/>
+        </Route>
+      </Routes>
+  );
+}
+
+export default App;
