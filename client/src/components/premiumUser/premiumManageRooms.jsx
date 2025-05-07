066e1811488a44f9e3075baf73531107f42564c3
udon95 <vanessawee2000@gmail.com>
2025-05-07 19:33:36 +0800

    Update premiumManageRooms.jsx

--- client/src/components/premiumUser/premiumManageRooms.jsx
+++ client/src/components/premiumUser/premiumManageRooms.jsx
@@ -396,7 +396,7 @@ const ManageRooms = () => {
         </section>
 
         {showModal && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="fixed inset-0 bg-opacity-50 flex items-center justify-center z-50">
             <div className="bg-white p-6 rounded-xl shadow-md w-full max-w-md space-y-4">
               <h2 className="text-xl font-semibold">Update Room</h2>
 
