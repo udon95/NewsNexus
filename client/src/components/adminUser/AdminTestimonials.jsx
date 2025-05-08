af85611059b09825acfc49a33133f0f9483b75f5
Toh Leng <tsuruyajin@gmail.com>
2025-05-09 01:40:26 +0800

    .

--- client/src/components/adminUser/AdminTestimonials.jsx
+++ client/src/components/adminUser/AdminTestimonials.jsx
@@ -145,44 +145,46 @@ const AdminTestimonials = () => {
 
           <div>
             {displayedRows.length > 0 ? (
-              <table className="min-w-full bg-gray-100 rounded-2xl shadow-lg text-left">
-              <thead className="bg-gray-200">
-                <tr>
-                  <th className="p-3">#</th>
-                  <th className="p-3">Username</th>
-                  <th className="p-3">Status</th>
-                  <th className="p-3">Submitted</th>
-                  <th className="p-3">Design</th>
-                  <th className="p-3">Factcheck accuracy</th>
-                  <th className="p-3">Accessibility</th>
-                  <th className="p-3">Safety</th>
-                  <th className="p-3">Pricing</th>
-                  <th className="p-3">News Quality</th>
-
-                </tr>
-              </thead>
-              <tbody>
-                {displayedRows.map((row, index) => (
-                  <tr
-                    key={row.id}
-                    className="cursor-pointer hover:bg-gray-300 transition-colors"
-                    onClick={() => openReport(row)}
-                  >
-                    <td className="p-3">{index + 1}</td>
-                    <td className="p-3">{users.find((user) => user.userid === row.userid)?.username || "Unknown"}</td>
-                    <td className="p-3">{row.homepage_display}</td>
-                    <td className="p-3">{row.submitted}</td>
-                    <td className="p-3">{row.design}</td>
-                    <td className="p-3">{row.factcheck}</td>
-                    <td className="p-3">{row.accessible}</td>
-                    <td className="p-3">{row.safety}</td>
-                    <td className="p-3">{row.price}</td>
-                    <td className="p-3">{row.news}</td>
+              <div className="overflow-x-auto ml-10 mt-8 max-w-4xl">
+                <table className="min-w-full bg-gray-100 rounded-2xl shadow-lg text-left">
+                <thead className="bg-gray-200">
+                  <tr>
+                    <th className="p-3">#</th>
+                    <th className="p-3">Username</th>
+                    <th className="p-3">Status</th>
+                    <th className="p-3">Submitted</th>
+                    <th className="p-3">Design</th>
+                    <th className="p-3">Factcheck</th>
+                    <th className="p-3">Access</th>
+                    <th className="p-3">Safety</th>
+                    <th className="p-3">Pricing</th>
+                    <th className="p-3">News Quality</th>
 
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {displayedRows.map((row, index) => (
+                    <tr
+                      key={row.id}
+                      className="cursor-pointer hover:bg-gray-300 transition-colors"
+                      onClick={() => openReport(row)}
+                    >
+                      <td className="p-3">{index + 1}</td>
+                      <td className="p-3">{users.find((user) => user.userid === row.userid)?.username || "Unknown"}</td>
+                      <td className="p-3">{row.homepage_display}</td>
+                      <td className="p-3">{row.submitted}</td>
+                      <td className="p-3">{row.design}</td>
+                      <td className="p-3">{row.factcheck}</td>
+                      <td className="p-3">{row.accessible}</td>
+                      <td className="p-3">{row.safety}</td>
+                      <td className="p-3">{row.price}</td>
+                      <td className="p-3">{row.news}</td>
+
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
             ) : (
               <div className="ml-10 mt-8">0 results</div>
             )}
