b9d45407acc4259dc965bb5219b12319f62d8ab3
udon95 <vanessawee2000@gmail.com>
2025-05-10 18:50:24 +0800

    Update register.jsx

--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -377,7 +377,7 @@ function Register() {
                     style={{ backdropFilter: "blur(4px)" }}
                     className="fixed inset-0 bg-opacity-50 z-50 flex items-center justify-center"
                   >
-                    <div className="bg-white w-11/12 max-w-4xl p-6 rounded-lg relative overflow-y-auto max-h-[90vh]">
+                    <div className="bg-white w-11/12 max-w-2xl rounded-lg relative">
                       <button
                         onClick={() => setShowGuidelinesModal(false)}
                         className="absolute top-4 right-4 text-gray-500 hover:text-gray-800 z-10"
@@ -398,7 +398,7 @@ function Register() {
                     style={{ backdropFilter: "blur(4px)" }}
                     className="fixed inset-0 bg-opacity-50 z-50 flex items-center justify-center"
                   >
-                    <div className="bg-white w-11/12 max-w-4xl p-6 rounded-lg relative overflow-y-auto max-h-[90vh]">
+                    <div className="bg-white w-11/12 max-w-2xl rounded-lg relative">
                       <button
                         onClick={() => setShowPrivacyModal(false)}
                         className="absolute top-4 right-4 text-gray-500 hover:text-gray-800 z-10"
