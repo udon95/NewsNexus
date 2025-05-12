64583434855a6cd6018b56f598a1a3f2a1d7e447
udon95 <vanessawee2000@gmail.com>
2025-05-13 02:08:50 +0800

    Update premiumManageProfile.jsx

--- client/src/components/premiumUser/premiumManageProfile.jsx
+++ client/src/components/premiumUser/premiumManageProfile.jsx
@@ -502,6 +502,18 @@ const PremManageProfile = () => {
                 className="w-full p-2 border rounded-lg"
               />
             </div>
+            {expertTopics.length > 0 && (
+              <div>
+                <label className="block mb-1 text-sm font-medium">
+                  Expert Topics:
+                </label>
+                <ul>
+                  {expertTopics.map((topic, index) => (
+                    <li key={index}>{topic}</li> // Display only the topic name
+                  ))}
+                </ul>
+              </div>
+            )}
           </div>
 
           {/* Right: Profile Fields */}
@@ -586,19 +598,6 @@ const PremManageProfile = () => {
                   className="w-full p-2 border rounded-lg bg-gray-100 text-gray-800"
                 />
               </div>
-
-              {expertTopics.length > 0 && (
-                <div>
-                  <label className="block mb-1 text-sm font-medium">
-                    Expert Topics:
-                  </label>
-                  <ul>
-                    {expertTopics.map((topic, index) => (
-                      <li key={index}>{topic}</li> // Display only the topic name
-                    ))}
-                  </ul>
-                </div>
-              )}
             </div>
 
             <button
