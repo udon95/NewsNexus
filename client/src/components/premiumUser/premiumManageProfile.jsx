b055170a619c3182e17b0973e716823f2e5d2dc4
udon95 <vanessawee2000@gmail.com>
2025-05-13 03:08:36 +0800

    U

--- client/src/components/premiumUser/premiumManageProfile.jsx
+++ client/src/components/premiumUser/premiumManageProfile.jsx
@@ -115,10 +115,10 @@ const PremManageProfile = () => {
             if (topicError) {
               console.error("Error fetching topic name:", topicError);
             } else {
-              topicNames.push({
+              topicNames.push(
                 //topicid: expert.topicid,
-                topicName: topicData?.name || "Unknown",
-              });
+                topicData?.name || "Unknown"
+              );
             }
           }
 
@@ -502,6 +502,19 @@ const PremManageProfile = () => {
                 className="w-full p-2 border rounded-lg"
               />
             </div>
+
+            {expertTopics.length > 0 && (
+              <div>
+                <label className="block mb-1 text-sm font-medium">
+                  Expert Topics:
+                </label>
+                <ul>
+                  {expertTopics.map((topic, index) => (
+                    <li key={index}>{topic}</li> // Display only the topic name
+                  ))}
+                </ul>
+              </div>
+            )}
           </div>
 
           {/* Right: Profile Fields */}
@@ -586,19 +599,6 @@ const PremManageProfile = () => {
                   className="w-full p-2 border rounded-lg bg-gray-100 text-gray-800"
                 />
               </div>
-
-              {expertTopics.length > 0 && (
-                <div>
-                  <label className="block mb-1 text-sm font-medium">
-                    Expert Topics:
-                  </label>
-                  <ul>
-                    {expertTopics.map((topic, index) => (
-                      <li key={index}>{topic}</li> // Display only the topic name
-                    ))}
-                  </ul>
-                </div>
-              )}
             </div>
 
             <button
