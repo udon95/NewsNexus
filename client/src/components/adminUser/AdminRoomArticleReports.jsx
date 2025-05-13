adecb2ae3a76c2629831de7fa85d391d30d3c0f3
Toh Leng <tsuruyajin@gmail.com>
2025-05-13 12:51:01 +0800

    .

--- client/src/components/adminUser/AdminRoomArticleReports.jsx
+++ client/src/components/adminUser/AdminRoomArticleReports.jsx
@@ -137,7 +137,7 @@ const AdminRoomArticleReports = () => {
                 {article && article.Suspended ? "Suspended" : "Active"}
               </div>
               <textarea className="ml-10 mt-5 max-w-[500px] bg-gray-100 rounded-2xl p-3 text-lg shadow-lg outline-none focus:ring-2 focus:ring-gray-300">
-                {selectedItem ? selectedItem.text : "No text in article"}
+                {selectedItem && selectedItem.content ? selectedItem.content : "No text in article"}
               </textarea>
               <div className="flex">
                 <button
