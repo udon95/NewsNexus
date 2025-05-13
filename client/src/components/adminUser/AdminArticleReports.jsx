cf85de17878ddfe9a077a2eee4e164ff184fd71e
Toh Leng <tsuruyajin@gmail.com>
2025-05-13 11:40:28 +0800

    .

--- client/src/components/adminUser/AdminArticleReports.jsx
+++ client/src/components/adminUser/AdminArticleReports.jsx
@@ -138,6 +138,9 @@ const AdminArticleReports = () => {
                 Article Status : &emsp;
                 {article && article.Suspended ? "Suspended" : "Active"}
               </div>
+              <textarea className="ml-10 mt-5 max-w-[500px] bg-gray-100 rounded-2xl p-3 text-lg shadow-lg outline-none focus:ring-2 focus:ring-gray-300">
+                {article ? article.text : "No text in article"}
+              </textarea>
               <div className="flex">
                 <button
                   type="button"
