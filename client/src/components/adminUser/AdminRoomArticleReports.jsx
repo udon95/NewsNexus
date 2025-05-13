ea13ba015beaf256e18a1a48d7a318dbaffc4707
Toh Leng <tsuruyajin@gmail.com>
2025-05-13 15:50:54 +0800

    .

--- client/src/components/adminUser/AdminRoomArticleReports.jsx
+++ client/src/components/adminUser/AdminRoomArticleReports.jsx
@@ -130,8 +130,7 @@ const AdminRoomArticleReports = () => {
               </div>
               <div className="ml-10 mt-5 max-w-[700px] bg-gray-100 rounded-2xl p-3 text-lg shadow-lg outline-none focus:ring-2 focus:ring-gray-300">
                 <div className="font-semibold mb-1">Article Title :</div>
-                <div className="underline cursor-pointer text-blue-600"  
-                    onClick={() => articleRedirect()}>
+                <div className="underline">
                   {article ? article.title : ""}
                 </div>
               </div>
