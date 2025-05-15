34f8cb59099e9f27c3356b87a38325ec26405925
udon95 <vanessawee2000@gmail.com>
2025-05-15 17:11:24 +0800

    U

--- client/src/components/premiumUser/premiumDashboard.jsx
+++ client/src/components/premiumUser/premiumDashboard.jsx
@@ -8,8 +8,6 @@ import PremiumWriteArticle from "./premiumWriteArticle.jsx";
 import PremiumManageProfile from "./premiumManageProfile.jsx";
 import PremiumManageRooms from "./premiumManageRooms.jsx";
 import PremiumEditArticle from "./premiumEditArticle";
-import RoomPage from "./roomPage.jsx";
-
 
 function PremiumDashboard() {
   return (
@@ -20,11 +18,7 @@ function PremiumDashboard() {
         </aside>
         <div className="flex-1 min-h-full bg-indigo-50 max-md:w-full w-full max-w-screen px-1 md:px-6">
           <Routes>
-            {/* Default Route: Redirect to Manage Articles */}
             <Route index element={<Navigate to="manageProfile" />} />
-
-            {/* Manage My Articles (Default View) */}
-            {/* <Route path="manageArticles" element={<PremManageArticle />} /> */}
 
             {/* Other Sidebar Pages */}
             <Route path="manageArticles" element={<PremManageArticle />} />
@@ -32,13 +26,10 @@ function PremiumDashboard() {
             <Route path="edit/:id" element={<PremiumEditArticle />} />
 
             <Route path="writeArticle/:id" element={<PremiumWriteArticle />} />
-            <Route path="edit/:id" element={<PremiumEditArticle />} />
             <Route path="manageProfile" element={<PremiumManageProfile />} />
             <Route path="submitTestimonial" element={<PremiumSubmitTest />} />
-            {/* <Route path="manageRooms" element={<PremiumManageRooms />} /> */}
             <Route path="manageRooms" element={<PremiumManageRooms />} />
             <Route path="applyExpert" element={<PremiumApplyExpert />} />
-           
           </Routes>
         </div>
       </main>
