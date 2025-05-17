dded1a5291314d8e065ff0890e47c586e993fe29
udon95 <45646895+udon95@users.noreply.github.com>
2025-05-17 11:08:33 +0800

    Revert "Update roomPage.jsx"
    This reverts commit 301e1500d4a2797b0832863b4f3ca12ff875a1d0.

--- client/src/components/premiumUser/roomPage.jsx
+++ client/src/components/premiumUser/roomPage.jsx
@@ -321,7 +321,6 @@ const Room = () => {
           .from("expert_application")
           .select("status")
           .eq("userid", userid)
-          .eq("topicid", data.topicid)
           .eq("status", "Approved");
 
         const isExpert = expertData?.length > 0;
